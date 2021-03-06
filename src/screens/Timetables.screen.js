c8d9b835c372b11c1e87a57d38183ce676c31e96
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-03-06 12:00:11 +0400

    add timetable search

--- src/screens/Timetables.screen.js
+++ src/screens/Timetables.screen.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import { withDatabase } from '@nozbe/watermelondb/DatabaseProvider';
 import withObservables from '@nozbe/with-observables';
+import styled from 'styled-components/native';
 import List from '../components/List';
 import CreationButton from '../components/CreationButton';
+import SearchInput from '../components/form/elements/SearchInput';
 
 const TimetablesScreen = ({ navigation, timetables }) => {
+  const [items, setItems] = useState([]);
+  const [search, setSearch] = useState('');
+
   const navigateToTimetable = (id) => {
     navigation.navigate('Timetable', { id });
   };
 
+  useEffect(() => {
+    setItems(
+      timetables.filter(({ name }) =>
+        name.toLowerCase().includes(search.toLowerCase()),
+      ),
+    );
+  }, [timetables, search]);
+
   return (
     <View style={{ flex: 1, justifyContent: 'space-between' }}>
+      <SearchContainer>
+        <SearchInput
+          value={search}
+          onChangeText={setSearch}
+          placeholder="Название расписания"
+        />
+      </SearchContainer>
       <List
-        items={timetables}
+        items={items}
         style={{
           flex: 1,
           top: -50,
@@ -30,6 +50,13 @@ const TimetablesScreen = ({ navigation, timetables }) => {
     </View>
   );
 };
+
+const SearchContainer = styled.View`
+  padding-horizontal: 15px;
+  padding-top: 20px;
+  padding-bottom: 70px;
+  background: #343434;
+`;
 
 export default withDatabase(
   withObservables([], ({ database }) => ({
