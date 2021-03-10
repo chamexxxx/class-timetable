79d69112f6b2d5474b51fbc47365a61beb9496af
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-03-10 20:49:29 +0400

    fix timetable form

--- src/components/form/TimetableForm.js
+++ src/components/form/TimetableForm.js
@@ -1,39 +1,25 @@
 import React, { useState } from 'react';
-import useValidation from '../../hooks/validation';
 import Form from './Form';
 import Field from './Field';
 import TextInput from './elements/TextInput';
 
 export default ({ onSubmit }) => {
   const [name, setName] = useState(null);
-  const { validate, errors } = useValidation(
-    {
-      name: { required: true, message: 'Введите название расписания' },
-    },
-    { name },
-  );
-
-  const _onSubmit = () => {
-    const [isError] = validate();
-
-    if (!isError) {
-      onSubmit({ name });
-    }
-  };
 
   return (
-    <Form onSubmit={_onSubmit}>
-      <Field
-        render={() => (
-          <TextInput
-            onChangeText={setName}
-            value={name}
-            placeholder="Название расписания"
-            autoFocus={true}
-          />
-        )}
-        errorMessage={errors.name && errors.name[0].message}
-      />
+    <Form
+      onSubmit={() => onSubmit({ name })}
+      buttonTitle="Создать"
+      buttonDisabled={!name}>
+      <Field>
+        <TextInput
+          onChangeText={setName}
+          value={name}
+          placeholder="Название расписания"
+          required
+          autoFocus
+        />
+      </Field>
     </Form>
   );
 };
