dd8997d353addc32490df311dd1931685772a6a3
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-03-13 18:26:35 +0400

    add splash screen

--- App.js
+++ App.js
@@ -5,15 +5,22 @@
  * @format
  * @flow strict-local
  */
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import DatabaseProvider from '@nozbe/watermelondb/DatabaseProvider';
 import database from './src/database';
 import Navigation from './src/navigation';
 
-const App: () => React$Node = () => (
-  <DatabaseProvider database={database}>
-    <Navigation />
-  </DatabaseProvider>
-);
+const App: () => React$Node = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <DatabaseProvider database={database}>
+      <Navigation />
+    </DatabaseProvider>
+  );
+};
 
 export default App;
