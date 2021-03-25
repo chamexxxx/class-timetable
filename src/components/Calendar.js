20d56e618b976e8662b7151f97c25c04744627e4
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-03-25 13:31:57 +0400

    make forwardref in calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import moment from 'moment';
 import locale from 'moment/locale/ru';
 import CalendarStrip from 'react-native-calendar-strip';
@@ -26,42 +26,47 @@ const customDatesStylesFunc = (date) => {
   };
 };
 
-export default React.forwardRef((props, ref) => (
-  <CalendarStrip
-    scrollable
-    ref={ref}
-    selectedDate={moment()}
-    style={{
-      height: 120,
-      paddingTop: 10,
-      borderBottomLeftRadius: 20,
-      borderBottomRightRadius: 20,
-    }}
-    calendarColor="#ffffff"
-    calendarHeaderStyle={{
-      marginBottom: 5,
-      color: '#000000',
-      fontSize: 15,
-      textTransform: 'uppercase',
-    }}
-    dayComponentHeight={50}
-    dayContainerStyle={{
-      borderRadius: 10,
-    }}
-    dateNumberStyle={{ color: '#000000' }}
-    dateNameStyle={{ color: '#000000', fontSize: 13 }}
-    highlightDateContainerStyle={{
-      backgroundColor: '#857cbd',
-    }}
-    highlightDateNumberStyle={{ color: '#fff' }}
-    highlightDateNameStyle={{ color: '#fff', fontSize: 13 }}
-    iconContainer={{ flex: 0.12 }}
-    calendarAnimation={{ type: 'sequence', duration: 0 }}
-    locale={{
-      name: 'ru',
-      config: locale,
-    }}
-    customDatesStyles={customDatesStylesFunc}
-    {...props}
-  />
-));
+export default React.forwardRef(({ date, onDateSelected, ...props }, ref) => {
+  const calendar = useRef(null);
+
+  return (
+    <CalendarStrip
+      scrollable
+      ref={calendar}
+      onDateSelected={onDateSelected}
+      selectedDate={moment()}
+      style={{
+        height: 120,
+        paddingTop: 10,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+      }}
+      calendarColor="#ffffff"
+      calendarHeaderStyle={{
+        marginBottom: 5,
+        color: '#000000',
+        fontSize: 15,
+        textTransform: 'uppercase',
+      }}
+      dayComponentHeight={50}
+      dayContainerStyle={{
+        borderRadius: 10,
+      }}
+      dateNumberStyle={{ color: '#000000' }}
+      dateNameStyle={{ color: '#000000', fontSize: 13 }}
+      highlightDateContainerStyle={{
+        backgroundColor: '#857cbd',
+      }}
+      highlightDateNumberStyle={{ color: '#fff' }}
+      highlightDateNameStyle={{ color: '#fff', fontSize: 13 }}
+      iconContainer={{ flex: 0.12 }}
+      calendarAnimation={{ type: 'sequence', duration: 0 }}
+      locale={{
+        name: 'ru',
+        config: locale,
+      }}
+      customDatesStyles={customDatesStylesFunc}
+      {...props}
+    />
+  );
+});
